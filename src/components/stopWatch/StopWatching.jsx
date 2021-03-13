e247f91b6618979e6c32ae60e40434bf60f71767
Kayot88 <kayot88@gmail.com>
2021-03-13 22:15:23 +0200

    change from dispatch to setState

--- src/components/stopWatch/StopWatching.jsx
+++ src/components/stopWatch/StopWatching.jsx
@@ -1,31 +1,11 @@
 import React, { useEffect, useReducer, useRef, useState } from "react";
 
-const reducer = (state, { type, payload }) => {
-  switch (type) {
-    case "START_CLICK":
-      return {
-        ...state,
-        count: payload.now - payload.startTime,
-      };
-    case "SETLOOP":
-      return {
-        ...state,
-        isLoop: !state.isLoop,
-      };
-    case "CLEAR":
-      return {
-        ...state,
-        isLoop: false,
-        count: 0
-      };
-
-    default:
-      return state;
-  }
+const reducer = (prevStates, newState) => {
+  return { ...prevStates, ...newState };
 };
 
 const StopWatching = () => {
-  const [{ isLoop, count }, dispatch] = useReducer(reducer, {
+  const [{ isLoop, count }, setState] = useReducer(reducer, {
     isLoop: false,
     count: 0,
   });
@@ -42,18 +22,20 @@ const StopWatching = () => {
     } else {
       const startTime = Date.now() - count;
       intervalRef.current = setInterval(() => {
-        dispatch({
-          type: "START_CLICK",
-          payload: { now: Date.now(), startTime },
+        setState({
+          count: Date.now() - startTime,
         });
       }, 0);
     }
-    dispatch({type: "SETLOOP"})
+    setState({ isLoop: !isLoop });
   }
 
   function handleClearClick() {
     clearInterval(intervalRef.current);
-    dispatch({type:"CLEAR"})
+    setState({
+      isLoop: false,
+      count: 0,
+    });
   }
   return (
     <div
